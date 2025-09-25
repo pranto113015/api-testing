d6508be7f5a0ff9178e80dac9ba789f2afd9c39c
pranto113015 <pranto3876@gmail.com>
2025-09-26 02:54:50 +0600

    Update welcome message in HomePage component to reflect Api Testing context

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to the Api Testing Home Page</h1>
     </div>
   );
 };
